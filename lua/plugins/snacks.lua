9fd38b10396e97a48b68554abc9e2e51c72c15bf
Aina Rajaonah Andriamanalimanda <aandriam@c1r4s13.42antananarivo.mg>
2025-03-15 20:05:47 +0300

    dashboard from snack.lua modified

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,17 +3,67 @@ return {
 priority = 1000,
 lazy = false,
   opts = {
-    bigfile = { enabled = true },
-    dashboard = { enabled = false },
-    explorer = { enabled = true },
+    bigfile = { enabled = false},
+    dashboard = {
+	    preset = {
+    keys = {
+      { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+      { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+      { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+      { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+    },
+    header =[[
+           ┊       ┊   ┊    ┊        ┊                                                                              
+           ┊       ┊   ┊   ˚★⋆｡˚  ⋆                                 　✦　˚                                          
+           ┊       ┊   ⋆                                                                       ˚　                  
+           ┊       ★⋆                                                                             　.　             
+           ┊ ◦          █████╗ ██╗███╗   ██╗ █████╗  ███╗ ███████╗    ██╗   ██╗██╗███╗   ███╗                       
+           ┊ .  ˚.     ██╔══██╗██║████╗  ██║██╔══██╗██╔██╗██╔════╝    ██║   ██║██║████╗ ████║                       
+          ˚★.          ███████║██║██╔██╗ ██║███████║╚═╝╚═╝███████╗    ██║   ██║██║██╔████╔██║        ╱|、           
+                       ██╔══██║██║██║╚██╗██║██╔══██║      ╚════██║    ╚██╗ ██╔╝██║██║╚██╔╝██║       (˚ˎ 。7         
+                       ██║  ██║██║██║ ╚████║██║  ██║      ███████║     ╚████╔╝ ██║██║ ╚═╝ ██║       |、˜            
+                       ╚═╝  ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝      ╚══════╝      ╚═══╝  ╚═╝╚═╝     ╚═╝       じしˍ,)ノ       
+
+                                   ˚　.　　　　　   . ✦　　　                          　˚　.                       
+                                           　✦　　 　.　　.　　　✦　˚ 　  ˚       ˚   　✦　　　.　                  
+    ]],
+  },
+  sections = {
+	  {section = "header"},
+    { section = "keys", gap = 1 },
+    {
+	pane = 2,
+	icon = " ",
+	title = "Projects",
+	section = "projects",
+	indent = 2,
+	padding = 2,
+},
+    {
+      pane = 2,
+      icon = " ",
+      title = "Git Status",
+      section = "terminal",
+      enabled = function()
+        return Snacks.git.get_root() ~= nil
+      end,
+      cmd = "git --no-pager diff --stat -B -M -C",
+      height = 5,
+      padding = 1,
+      ttl = 5 * 60,
+      indent = 3,
+    },
+  },
+    },
+    explorer = { enabled = false },
     indent = { enabled = false },
-    input = { enabled = true },
-    picker = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
+    input = { enabled =  false},
+    picker = { enabled = false },
+    notifier = { enabled = false },
+    quickfile = { enabled =  false},
     scope = { enabled = false },
     scroll = { enabled = false },
     statuscolumn = { enabled = false },
     words = { enabled = false },
-  },
+},
 }
