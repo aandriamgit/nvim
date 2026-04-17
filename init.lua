fece7a36fc1becb41001ed520a08517dda876698
Aina Rajaonah Andriamanalimanda <aandriam@c1r7s14.42antananarivo.mg>
2026-04-17 07:49:26 +0300

    i have no idea what changed

--- init.lua
+++ init.lua
@@ -1,16 +1,22 @@
 require("config.lazy")
+vim.g.copilot_no_tab_map = true
 vim.opt.guicursor = ""
 vim.opt.number = true
 vim.keymap.set("n", "<leader>/", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 vim.keymap.set("n", "F", ":CFormateur42<CR> <BAR> :w<CR>")
-
-
--- disable netrw at the very start of your init.lua
+vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP Code Actions' })
+vim.keymap.set("i", "<C-p>", 'copilot#Accept("<CR>")', {
+  expr = true,
+  silent = true,
+  replace_keycodes = false,
+})
+vim.keymap.set("n", "<leader>cd", ":Copilot disable<CR>", { desc = "Copilot Disable" })
+vim.keymap.set("n", "<leader>ce", ":Copilot enable<CR>", { desc = "Copilot Enable" })
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.g.python3_host_prog = vim.fn.expand('~/.pyenv/shims/python')
 vim.opt.scrolloff = 4
--- optionally enable 24-bit colour
 vim.opt.termguicolors = true
 vim.api.nvim_create_user_command("F", function()
 vim.cmd("CFormatter42")
@@ -38,7 +44,7 @@ vim.keymap.set('n', '<A-l>', '<C-w>l')
 vim.keymap.set('n', '<A-right>', '<C-w>l')
 vim.keymap.set('n', '<leader>vs', ':vsplit<CR>', { desc = 'Vertical Split' })
 vim.cmd("set termguicolors")
-vim.cmd("highlight WinSeparator guifg=#24273a")
+vim.cmd("highlight WinSeparator guifg=#24273A guibg=NONE")
 
 local api = require("nvim-tree.api")
 vim.api.nvim_create_autocmd("BufEnter", {
@@ -50,6 +56,6 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end,
 })
 vim.diagnostic.config({
-  virtual_text = true,  -- Affiche les erreurs en ligne
+  virtual_text = true,
   underline = true,
 })
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,8 +14,10 @@ local mode_map = {
   ["Vs"] = "૮ ˶´ ᵕˋ ˶ა",
   ["�s"] = "V-BLOCK",
   ["s"] = "SELECT",
+  ["s"] = "V-BLOCK",
   ["S"] = "S-LINE",
   ["�"] = "S-BLOCK",
+  [""] = "S-BLOCK",
   ["i"] = "≽/ᐠ - ˕ -マ≼ Ⳋ",
   ["ic"] = "INSERT",
   ["ix"] = "INSERT",
@@ -40,66 +42,48 @@ local function custom_mode()
   return mode_map[mode] or mode
 end
 
-local custom_catp = require("lualine.themes.catppuccin")
-custom_catp.normal.c.bg = "#24273a"
-custom_catp.normal.b.bg = "#363a4f"
-custom_catp.normal.a.fg = "#24273a"
-
 return {
     {
         "nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
+        dependencies = { 
+            "nvim-tree/nvim-web-devicons",
+            "catppuccin/nvim"
+        },
         opts = {
             options = {
-		globalstatus = true,
+		theme = "catppuccin",
+                globalstatus = true,
                 component_separators = "",
                 section_separators = { left = "", right = "" },
-	        theme = custom_catp,
-		disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+                disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
             },
             sections = {
-		lualine_a = {
-		 	{ custom_mode, separator = { left = "" }, right_padding = 2 },
-		 },
-        lualine_b = { "branch" },
-        lualine_c = {
-          {
-            "diagnostics",
-            symbols = {
-              error = " ",
-              warn = " ",
-              info = " ",
-              hint = " ",
-            },
-          },
-          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-          {
-            "filename",
-            path = 1,
-            symbols = { modified = "  ", readonly = "  " }
-          }
-        },
-        lualine_x = {
-          {
-            "diff",
-            symbols = {
-              added = " ",
-              modified = " ",
-              removed = " ",
-            },
-          },
-        },
+                lualine_a = {
+                    { custom_mode, separator = { left = "" }, right_padding = 2 },
+                },
+                lualine_b = { "branch" },
+                lualine_c = {
+                  {
+                    "diagnostics",
+                    symbols = { error = " ", warn = " ", info = " ", hint = " " },
+                  },
+                  { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                  { "filename", path = 1, symbols = { modified = "  ", readonly = "  " } }
+                },
+                lualine_x = {
+                  {
+                    "diff",
+                    symbols = { added = " ", modified = " ", removed = " " },
+                  },
+                },
                 lualine_y = {
                     { "filetype", separator = " ", padding = { left = 1, right = 0 } },
                     { "location", padding = { left = 0, right = 1 } },
                 },
                 lualine_z = {
-                    {
-			"mode",
-                        separator = { right = "" },
-                    },
+                    { "mode", separator = { right = "" } },
                 },
             },
         },
-    },
+},
 }
